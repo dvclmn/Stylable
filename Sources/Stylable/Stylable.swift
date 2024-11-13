0afd61fe204ee3d8bb58cd4c3d8505ee707eb581
Dave Coleman <dave@dvclmn.com>
2024-11-13 11:48:12 +0100

    Trying to figure out best approach to entry point

--- Sources/Stylable/Stylable.swift
+++ Sources/Stylable/Stylable.swift
@@ -1,6 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import SwiftUI
 
 /// A protocol that defines an element that can be styled using a `StyleConfiguration`.
 ///
@@ -42,7 +43,24 @@ public extension Stylable {
   //    modified { $0 = newConfig }
   //  }
   
-  func with(_ newConfig: StyleConfiguration) -> Self {
-    modified { $0 = newConfig }
+  func with(_ config: StyleConfiguration) -> Self {
+    modified { $0 = config }
   }
 }
+
+
+
+
+public extension View where Self: Stylable {
+  
+  func stylable<S>(_ config: S) -> some View where S == Self.StyleConfiguration {
+    return self.modified { $0 = config }
+  }
+}
+
+//public struct StylerModifier: ViewModifier {
+//  
+//  public func body(content: Content) -> some View {
+//    content
+//  }
+//}
